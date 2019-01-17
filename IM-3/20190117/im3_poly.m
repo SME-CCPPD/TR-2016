ea9b83328c8e18e14ced36602dfe3d99afc06ce8
Francesco Ganis <frandrums10@gmail.com>
2019-01-17 10:56:04 +0100

    - Siamo andati avanti

---
+++ IM-3/20190117/im3_poly.m
@@ -0,0 +1,31 @@
+%Creazione della parte inferiore del "contenitore".
+xlo = [1 1.2 2 3 3.9 4.6 5.4 6];
+ylo = [50 40 30 50 45 70 80 500]*2;
+plo = (polyfit(xlo, ylo, length(xlo)));
+X = [xlo(1):0.01:xlo(end)];
+Ylo = polyval(plo, X);
+
+
+
+%Creazione della parte superiore del "contenitore". 
+xhi = xlo; %Per comodità usiamo le stesse x.
+yhi = [50 150 400 550 650 700 650 500]*2;
+phi = (polyfit(xhi, yhi, length(xhi)));
+Yhi = polyval(phi, X);
+
+plot (X, Ylo, X, Yhi, xlo, ylo, "*", xhi, yhi, "*")
+
+%Stampa del file da leggere con Ruby, quindi utilizziamo la sua sintassi.
+fh = fopen("tables.rb", "w");  %Generazione del file di Ruby e apertura in scrittura. 
+fprintf (fh, "T = ["); %Inizio da stampare dell'array dopo il quale vanno scritti i valori.
+for k=1:length(X)
+fprintf (fh, "%.2f, ", X(k));
+endfor
+fprintf (fh, "]\n");   %Fine dell'array. 
+
+fprintf (fh, "F = [");
+for k=1:length(X)
+fprintf (fh, "[%.2f, %.2f], ", Ylo(k), Yhi(k));
+endfor
+fprintf (fh, "]\n");
+fclose (fh); 
